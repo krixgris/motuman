cff2443716a09aac84a64d9bd9c279a7a208ab52
jens <jens_aid@yahoo.se>
2023-05-19 19:47:20 +0200

    more init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::process;
+
+use motuman::Config;
+
 fn main() {
-    println!("Hello, world!");
+    // let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = motuman::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
