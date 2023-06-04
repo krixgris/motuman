d60c8a12c823c06eaa3f939c9d2cdeda4ea5c6e7
krixgris <jens_aid@yahoo.se>
2023-06-04 14:50:45 +0200

    basic fix for tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,28 @@ pub mod config;
 
 #[cfg(test)]
 mod tests {
-    use crate::{motu::{Channel, MotuCommand, Motu}};
-
+    use crate::{motu::{Channel, MotuCommand, Motu}, config::Config};
     use super::*;
-    use std::error::Error;
+    use std::{error::Error, collections::HashMap};
+    
+    fn get_mock_config() -> Config {
+        let mut mock_config = Config {
+            ip_address: String::from("127.0.0.1:8000"),
+            aux_channels: HashMap::new(),
+            channels: HashMap::new(),
+            monitor_groups: HashMap::new(),
+
+        };
+        mock_config.aux_channels.insert(1, String::from("Aux 1"));
+        mock_config.channels.insert(1, String::from("Channel 1"));
+        mock_config.monitor_groups.insert(1, String::from("Monitor Group 1"));
+        mock_config
+    }
 
     #[test]
     fn test_enable_monitoring() -> Result<(), Box<dyn Error>> {
-        let motu = Motu::new("127.0.0.1:8000")?;
+        let mock_config = get_mock_config();
+        let motu = Motu::new("127.0.0.1:8000", &mock_config)?;
         motu.send(MotuCommand::EnableMonitoring)?;
         // Add assertions here to check that monitoring is enabled
         Ok(())
@@ -18,7 +32,8 @@ mod tests {
 
     #[test]
     fn test_disable_monitoring() -> Result<(), Box<dyn Error>> {
-        let motu = Motu::new("127.0.0.1:8000")?;
+        let mock_config = get_mock_config();
+        let motu = Motu::new("127.0.0.1:8000", &mock_config)?;
         motu.send(MotuCommand::DisableMonitoring)?;
         // Add assertions here to check that monitoring is disabled
         Ok(())
@@ -26,7 +41,8 @@ mod tests {
 
     #[test]
     fn test_print_settings() -> Result<(), Box<dyn Error>> {
-        let motu = Motu::new("127.0.0.1:8000")?;
+        let mock_config = get_mock_config();
+        let motu = Motu::new("127.0.0.1:8000", &mock_config)?;
         motu.send(MotuCommand::PrintSettings)?;
         // Add assertions here to check that settings are printed
         Ok(())
@@ -34,7 +50,8 @@ mod tests {
 
     #[test]
     fn test_volume() -> Result<(), Box<dyn Error>> {
-        let motu = Motu::new("127.0.0.1:8000")?;
+        let mock_config = get_mock_config();
+        let motu = Motu::new("127.0.0.1:8000", &mock_config)?;
         motu.send(MotuCommand::Volume(Some(Channel::new(1)), 1.0))?;
         // Add assertions here to check that volume is set correctly
         Ok(())
@@ -42,7 +59,8 @@ mod tests {
 
     #[test]
     fn test_send() -> Result<(), Box<dyn Error>> {
-        let motu = Motu::new("127.0.0.1:8000")?;
+        let mock_config = get_mock_config();
+        let motu = Motu::new("127.0.0.1:8000", &mock_config)?;
         motu.send(MotuCommand::Send(Some(Channel::new(1)), Some(Channel::new(2)), 0.5))?;
         // Add assertions here to check that send is set correctly
         Ok(())
