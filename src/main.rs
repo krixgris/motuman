6135512c81185e305d63faad3634d58c0d911ab8
krixgris <jens_aid@yahoo.se>
2023-06-04 09:24:52 +0200

    clap added

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,28 @@ use std::env;
 use std::process;
 // use std::time::{SystemTime, UNIX_EPOCH};
 
+use clap::Parser;
+
 use motuman::config;
 use motuman::motu;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
+    let args = Args::parse();
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
     let config = config::Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
