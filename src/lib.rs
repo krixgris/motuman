fe719d04f9b0666023d8e18533b00f73bc8769a1
jens <jens_aid@yahoo.se>
2023-06-06 20:16:37 +0200

    fix test

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,26 @@
-pub mod motu;
 pub mod config;
+pub mod motu;
 
 #[cfg(test)]
 mod tests {
-    use crate::{motu::{Channel, MotuCommand, Motu, ChannelType}, config::Config};
-    use std::{error::Error, collections::HashMap};
-    
+    use crate::{
+        config::Config,
+        motu::{Channel, ChannelType, Motu, MotuCommand},
+    };
+    use std::{collections::HashMap, error::Error};
+
     fn get_mock_config() -> Config {
         let mut mock_config = Config {
             ip_address: String::from("127.0.0.1:8000"),
             aux_channels: HashMap::new(),
             channels: HashMap::new(),
             monitor_groups: HashMap::new(),
-
         };
         mock_config.aux_channels.insert(1, String::from("Aux 1"));
         mock_config.channels.insert(1, String::from("Channel 1"));
-        mock_config.monitor_groups.insert(1, String::from("Monitor Group 1"));
+        mock_config
+            .monitor_groups
+            .insert(1, String::from("Monitor Group 1"));
         mock_config
     }
 
@@ -60,7 +64,11 @@ mod tests {
     fn test_send() -> Result<(), Box<dyn Error>> {
         let mock_config = get_mock_config();
         let motu = Motu::new("127.0.0.1:8000", &mock_config)?;
-        motu.send(MotuCommand::Send(Channel::new(1, ChannelType::Chan), Channel::new(1f, ChannelType::Aux), 0.5))?;
+        motu.send(MotuCommand::Send(
+            Channel::new(1, ChannelType::Chan),
+            Channel::new(1, ChannelType::Aux),
+            0.5,
+        ))?;
         // Add assertions here to check that send is set correctly
         Ok(())
     }
